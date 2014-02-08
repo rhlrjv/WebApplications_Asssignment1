677c1c9a3705dc12095a83431b69feeca5390b42
Rahul Rajeev <rahulrajeev@nus.edu.sg>
2014-02-09 04:19:30 +0800

    updated some new pages. working with ui ux
    created login signup and add new todo pages. implemented the MVC
    structure and majority of the css required

--- Controller.php
+++ Controller.php
@@ -6,6 +6,24 @@
 	//if(isset($_REQUEST['restart']))
 	//	session_unset();
 	
+	$view = "ViewLogin.php";
+	
+	if(isset($_REQUEST['page'])){
+		switch ($_REQUEST['page']){
+			case "login":
+				$view = "viewLogin.php";
+				break;
+			case "signup":
+				$view = "viewSignup.php";
+				break;
+			case "todo":
+				$view = "viewTodo.php";
+				break;
+			case "addtodo":
+				$view = "viewAddTodo.php";
+				break;
+		}
+	}
 	if(isset($_REQUEST['submitToDo']))
 	{
 		$NewTodoName = $_REQUEST['TodoName'];
@@ -15,5 +33,5 @@
 	else
 		$NewTodoName = "";
 		
-	require 'ViewSignup.php'; //add this into a switch case statement to access different files
+	require $view; //add this into a switch case statement to access different files
 ?> 
--- ViewHeader.php
+++ ViewHeader.php
@@ -1,5 +1,11 @@
 <header>
 	<div class = "wrap">
 		<div class = "title">Todo Manager</div>
+		<nav>
+			<a <?php if($view == "viewLogin.php") echo("class = \"selected\""); ?> href = "?page=login">Log in</a>
+			<a <?php if($view == "viewSignup.php") echo("class = \"selected\""); ?> href = "?page=signup">Sign up</a>
+			<a <?php if($view == "viewTodo.php") echo("class = \"selected\""); ?> href = "?page=todo">Todo</a>
+			<a <?php if($view == "viewAddTodo.php") echo("class = \"selected\""); ?> href = "?page=addtodo">Add Todo</a>
+		</nav>
 	</div>
 </header>
