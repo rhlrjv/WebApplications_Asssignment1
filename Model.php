9c91cd04a7d3741fda60973d9c2dd6341cfa037f
Rahul Rajeev <rahulrajeev91@gmail.com>
2014-02-13 20:49:33 +0800

    added basic application states to the Model
    Added 2 main states : logged_in or logged_out

--- Model.php
+++ Model.php
@@ -1,4 +1,38 @@
 <?php
 	session_save_path("$session_save_path");
 	session_start(); // must be first thing in the php file
+	
+	if(!isset($_SESSION['AppState']))
+		$_SESSION['AppState'] = "l_out";
+
+	function clearSession()
+	{
+		session_unset();
+	}
+	
+	function changeState($toState)
+	{
+		switch($toState)
+		{
+			case "logged_in":
+				$_SESSION['AppState'] = "l_in";
+				return true;
+				break;
+			case "logged_out":
+				$_SESSION['AppState'] = "l_out";
+				return true;
+				break;
+			default:
+				return false;
+				break;
+		}
+	}
+	
+	function isLoggedIn()
+	{
+		if($_SESSION['AppState'] == "l_in")
+			return true;
+		else
+			return false;
+	}
 ?>
