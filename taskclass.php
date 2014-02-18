0a4a87f1b33642b8b657378f9609b463c308058a
shambavi92 <shambavi92@gmail.com>
2014-02-18 22:35:56 +0800

    moving dbconnect to model

--- taskclass.php
+++ taskclass.php
@@ -8,35 +8,15 @@
 		var $completedhrs_col = 'completedhrs';	//COMPLETED HOURS column
 		var $imp_col = 'important';				//IMPORTANT column
 		var $username_col = 'username';			//USERNAME column
-		
-		var $dbconn = '';
-		
-		//connect to database
-		function dbconnect()
-		{
-			$dbhostname = "localhost";
-			$dbport = "5432";
-			$dbname = $GLOBALS["db_name"];
-			$dbuser = $GLOBALS["db_user"];
-			$dbpwd = $GLOBALS["db_password"];
-			$connectionString = "host=$dbhostname port=$dbport dbname=$dbname user=$dbuser password=$dbpwd";
-			$this->dbconn = pg_connect($connectionString);
-			if(!$this->dbconn)
-			{
-				setMessage("Can't connect to the database");	
-				exit;
-			}
-			return;
-		}
-		
+				
 		//Add a new to do
-		function addtodo($argId, $argTaskname, $argTotalHrs, $argCompletedhrs, $argImp, $argUsername)
+		function addtodo($dbconn, $argId, $argTaskname, $argTotalHrs, $argCompletedhrs, $argImp, $argUsername)
 		{
-			if(($this->dbconn)  != '')
+			if(($dbconn)  != '')
 			{
 				$addtodo_query="INSERT INTO $this->task_table (id_col, taskname_col, totalhrs_col, completedhrs_col, imp_col, username_col) values($1, $2, $3, $4, $5, $6);";
-				$result = pg_prepare($this->dbconn, "my_query", $signup_query);
-				$result = pg_execute($this->dbconn, "my_query", array($argUsername, $argPassword, $argEmail, $argDob));
+				$result = pg_prepare($dbconn, "my_query", $signup_query);
+				$result = pg_execute($dbconn, "my_query", array($argUsername, $argPassword, $argEmail, $argDob));
 				if($result)
 					return true; 
 				else
--- userclass.php
+++ userclass.php
@@ -7,34 +7,15 @@
 		var $email_col = 'email';			//EMAIL column
 		var $dob_col = 'dob';				//DATE OF BIRTH column
 		
-		var $dbconn = '';
-		
-		//connect to database
-		function dbconnect()
-		{
-			$dbhostname = "localhost";
-			$dbport = "5432";
-			$dbname = $GLOBALS["db_name"];
-			$dbuser = $GLOBALS["db_user"];
-			$dbpwd = $GLOBALS["db_password"];
-			$connectionString = "host=$dbhostname port=$dbport dbname=$dbname user=$dbuser password=$dbpwd";
-			$this->dbconn = pg_connect($connectionString);
-			if(!$this->dbconn)
-			{
-				setMessage("Can't connect to the database");	
-				exit;
-			}
-			return;
-		}
 		
 		//Sign up as new user
-		function signup($argUsername, $argPassword, $argEmail, $argDob)
+		function signup($dbconn, $argUsername, $argPassword, $argEmail, $argDob)
 		{
-			if(($this->dbconn)  != '')
+			if($dbconn != '')
 			{
 				$signup_query="INSERT INTO $this->user_table ($this->username_col, $this->password_col, $this->email_col, $this->dob_col) values($1, $2, $3, $4);";
-				$result = pg_prepare($this->dbconn, "my_query", $signup_query);
-				$result = pg_execute($this->dbconn, "my_query", array($argUsername, $argPassword, $argEmail, $argDob));
+				$result = pg_prepare($dbconn, "my_query", $signup_query);
+				$result = pg_execute($dbconn, "my_query", array($argUsername, $argPassword, $argEmail, $argDob));
 				if($result)
 					return true; 
 				else
@@ -43,13 +24,13 @@
 		}
 		
 		//Login to app
-		function login($argUsername, $argPassword)
+		function login($dbconn, $argUsername, $argPassword)
 		{
-			if(($this->dbconn)  != '')
+			if($dbconn != '')
 			{
 				$login_query="SELECT * FROM $this->user_table where $this->username_col = $1 AND $this->password_col = $2;";
-				$result = pg_prepare($this->dbconn, "my_query", $login_query);
-				$result = pg_execute($this->dbconn, "my_query", array($argUsername,$argPassword));
+				$result = pg_prepare($dbconn, "my_query", $login_query);
+				$result = pg_execute($dbconn, "my_query", array($argUsername,$argPassword));
 				
 				if($result)
 				{
@@ -64,10 +45,10 @@
 		
 		function logincheck($aLogincode){
 		
-			if(($this->dbconn)  != ''){
+			if(($dbconn)  != ''){
 				$login_query="SELECT * FROM $this->user_table where $this->pass_column = $1;";
-				$result = pg_prepare($this->dbconn, "my_query", $login_query);
-				$result = pg_execute($this->dbconn, "my_query", array($aLogincode));
+				$result = pg_prepare($dbconn, "my_query", $login_query);
+				$result = pg_execute($dbconn, "my_query", array($aLogincode));
 				if(pg_num_rows($result)==0 || !($result)){
 					//echo "no result";
 					return false;
